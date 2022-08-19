d6f741b592217af956f9a705b26294d321e685d5
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-19 16:01:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+console.log(saturdayFun(activity = "attend church service"));
+function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+};
+console.log(saturdayFun(activity = "attend my coding lessons"));
+function wrapAdjective(initialized = "*"){
+    return function(pipeInitial = "special"){
+        return `You are ${initialized}${pipeInitial}${initialized}!`;
+    }
+}
+wrapAdjective()();
